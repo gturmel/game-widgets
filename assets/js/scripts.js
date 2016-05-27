b3bad10c23ffd6f776e535ae19926bb2e9ce8b22
Greg Turmel <gregturmel@gmail.com>
2016-05-27 12:01:19 -0400

    attacker and defender button logic

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,19 +5,57 @@ games.controller("riskController", ["$scope", function($scope){
 
    rng = function(){
       if (randomNumber > 0 && randomNumber <= 1/6){
-      console.log('1');
+         return 1;
       } else if (randomNumber > 1/6 && randomNumber <= 1/3) {
-         console.log('2');
+         return 2;
       } else if (randomNumber > 1/3 && randomNumber <= 0.5) {
-         console.log('3');
+         return 3;
       } else if (randomNumber > 0.5 && randomNumber <= 2/3){
-         console.log('4');
+         return 4;
       } else if (randomNumber > 2/3 && randomNumber <= 5/6) {
-         console.log('5');
+         return 5;
       } else {
-         console.log('6');
+         return 6;
       };
    };
-   rng();
-   
+
+   var attackerDie = document.getElementsByClassName("attacker-die");
+   var defenderDie = document.getElementsByClassName("defender-die");
+   var attackerButton = document.getElementsByClassName("attacker-button");
+   var defenderButton = document.getElementsByClassName("defender-button");
+   var attackerReady = false;
+   var defenderReady = false;
+   var attackCounter = 0;
+   var defenderCounter = 0;
+
+   attackerButton[0].addEventListener("click", function(){
+      attackerReady = !attackerReady;
+      if(attackCounter === 0){
+         attackerButton[0].classList.remove("btn-warning");
+         attackerButton[0].classList.add("btn-success");
+         attackerButton[0].textContent = "Attacker Ready!";
+         attackCounter++;
+      } else {
+         attackerButton[0].classList.remove("btn-success");
+         attackerButton[0].classList.add("btn-warning");
+         attackerButton[0].textContent = "Attacker Ready?";
+         attackCounter--;
+      }
+   });
+
+   defenderButton[0].addEventListener("click", function(){
+      defenderReady = !defenderReady;
+      if(defenderCounter === 0){
+         defenderButton[0].classList.remove("btn-warning");
+         defenderButton[0].classList.add("btn-success");
+         defenderButton[0].textContent = "Defender Ready!";
+         defenderCounter++;
+      } else {
+         defenderButton[0].classList.remove("btn-success");
+         defenderButton[0].classList.add("btn-warning");
+         defenderButton[0].textContent = "Defender Ready?"
+         defenderCounter--;
+      }
+   });
+
 }]);
